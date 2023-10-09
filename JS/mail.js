b6b5c05dfabcafbe531c7d6ec6d1cb991e6e7abf
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-09 15:34:03 +0200

    ADD: array mail

--- JS/mail.js
+++ JS/mail.js
@@ -1,1 +1,21 @@
 // console.log('ciao')
+
+
+// inserimento mail utente
+let inserisciMail = prompt ('Ciao coso/a! Inserisci la tua mail!');
+console.log("Mail inserità dall'utente " + inserisciMail)
+
+// lista di mail salvate nel database
+const arrayMailUtenti = ['luca@gmail.com', 'giorgio@gmail.com', 'filo@gmail.com', 'ele@gmail.com', 'ale@gmail.com']
+
+console.log(arrayMailUtenti);
+const lunghezzaMail = arrayMailUtenti.length;
+
+// ciclo di controllo delle mail
+for (let i = 0; i < arrayMailUtenti.length; i++) {
+    
+
+    const mailCorrente = arrayMailUtenti[i]
+    console.log(i ) 
+    
+ }
