848f5d222b07c7a143f2df3edbb50c57b525b3dc
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-09 18:51:43 +0200

    ADD: DomElement player and pc

--- JS/dadi.js
+++ JS/dadi.js
@@ -1,29 +1,54 @@
 // console.log('ciao')
 
-// Numeri casuali del giocatore
-const giocatore = Math.floor(Math.random() * (6 - 1) + 1);
-console.log('Player ' + giocatore)
-
-// Numeri casuali del computer
-const computer = Math.floor(Math.random() * (6 - 1) + 1);
-console.log('Bot ' + computer)
 
 
-let message
 
-if(giocatore > computer){
 
-    message = "Allora non sei così scarso"
-    console.log('hai vinto')
-} else if (giocatore < computer) {
-    message = 'Sei una sciappa'
-    console.log('hai perso')
-} else {
-    message = 'Riprova sarai più sfortunato'
-    console.log('pareggio')
-}
+const buttonDocElement = document.getElementById('button');
 
-alert(message)
+
+buttonDocElement.addEventListener ('click', function () {
+    // Numeri casuali del giocatore
+    const giocatore = Math.floor(Math.random() * (6 - 1) + 1);
+    console.log('Player ' + giocatore)
+
+    // Numeri casuali del computer
+    const computer = Math.floor(Math.random() * (6 - 1) + 1);
+    console.log('Bot ' + computer)
+
+
+    let message
+
+    if(giocatore > computer){
+
+        message = "Allora non sei così scarso"
+        console.log('hai vinto')
+    } else if (giocatore < computer) {
+        message = 'Sei una sciappa'
+        console.log('hai perso')
+    } else {
+        message = 'Riprova sarai più sfortunato'
+        console.log('pareggio')
+
+        // const playerDomElement 
+    }
+
+    let player = giocatore
+
+    const playerDomElement = document.getElementById('player');
+    playerDomElement.innerHTML = player
+
+    let bot = computer 
+
+    const botDomElement = document.getElementById('bot')
+    botDomElement.innerHTML = bot 
+
+    
+
+    // alert(message)
+})
+
+
 
 
 
