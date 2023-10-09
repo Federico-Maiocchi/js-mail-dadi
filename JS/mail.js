b84754c89684bb179681889359ae0803176a2b88
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-09 16:49:49 +0200

    ADD: message no accepted

--- JS/mail.js
+++ JS/mail.js
@@ -5,25 +5,31 @@
 let inserisciMail = prompt ('Ciao coso/a! Inserisci la tua mail!');
 console.log("Mail inserità dall'utente " + inserisciMail)
 
-let mailUser = inserisciMail.value
-console.log(mailUser)
+// let mailUser = inserisciMail.value
+// console.log(mailUser)
 
 // lista di mail salvate nel database
 const arrayMailUtenti = ['luca@gmail.com', 'giorgio@gmail.com', 'filo@gmail.com', 'ele@gmail.com', 'ale@gmail.com']
 
-console.log(arrayMailUtenti);
+// console.log(arrayMailUtenti);
 const lunghezzaMail = arrayMailUtenti.length;
+
+
+let mailNotacpt
+let message = 'Mi disp ma la non è stata accettata'
 
 // ciclo di controllo delle mail
 for (let i = 0; i < arrayMailUtenti.length; i++) {
     
     const mailCorrente = arrayMailUtenti[i]
-    console.log(i, mailCorrente )
+    // console.log(i, mailCorrente )
 
-    
-    
-    // if ( mailCorrente === mailUser ) {
-    //     console.log()
-    // }
-    
+    if ( inserisciMail === mailCorrente) {
+        message = 'Che fortuna! (per non dire altro) la tua mail corrisponde'
+        // alert('La tua mai è stata accettata')
+        // console.log('accepted')
+    }
  }
+
+ console.log(message)
+ alert(message)
